ed971b904f3e9198ed23c5f7d9fdb3b75494719b
SIMPLY-CHAOTIC <jithinbaby5@.com>
2024-07-06 09:47:34 +0530

    feature signup working

---
+++ full_body_tracking.py
@@ -0,0 +1,23 @@
+import mediapipe as mp
+import cv2
+mp_drawing = mp.solutions.drawing_utils
+mp_holistic = mp.solutions.holistic
+
+cap=cv2.VideoCapture(0)
+holistic = mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5)
+while cap.isOpened():
+    _, frame = cap.read()
+    frame = cv2.resize(frame, (1500, 1000))
+    frame=cv2.flip(frame, 1)
+    image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) 
+    results = holistic.process(image)
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_CONTOURS)
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS)
+    cv2.imshow('Raw Webcam Feed', image)
+    if cv2.waitKey(1)==27:
+        cap.release()
+        cv2.destroyAllWindows()
+        break
